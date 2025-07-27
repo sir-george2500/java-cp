23dddf893e1942fb3dd81a77f88a9f16ba67a63c
Nimely111 <samuelehnimely@gmail.com>
2025-07-27 17:22:26 +0000

    update issue #9 output

--- JavaIntroduction/Solution.java
+++ JavaIntroduction/Solution.java
@@ -2,12 +2,11 @@ public class Solution{
     public static void main(String[] args){
 // declare a char variable grade and assign it the character 'A' #9
         char grade = 'A';
-        System.out.println("Grade: "+ grade);
+        System.out.println("Issue #9 - Grade"+ grade);
 
 //        Issue #17 variable reassignment output
         issue17Output();
     }
-
 
 //    Issue #17
     public static void issue17Output(){
