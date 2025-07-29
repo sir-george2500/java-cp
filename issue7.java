de3626aa8b653b639747bd2ec94a1f95e9036db9
Albert Hills <ahills@users-MacBook-Air.local>
2025-07-29 17:08:24 -0400

    Save this issue7

---
+++ issue7.java
@@ -0,0 +1,3 @@
+// The difference between float and double in java are
+//float is a  primitive data types used to store single precision 32-bit floating point numbers.
+// Double is  a primitive data type used to represent double precision 64 bit IEEE 754 floating point numbers.
