5b16bd0efd0be67804065ba673e9dd4b94757315
Albert Hills <ahills@users-MacBook-Air.local>
2025-07-29 19:45:46 -0400

    save this issue34

---
+++ issue34.java
@@ -0,0 +1,5 @@
+//The difference between widening type casting and narrowing type casting
+//Widening type casting is actually known as implicit type casting in which a smaller type is converted into a larger type.
+// This  is done by the compiler automatically.
+//While the narrowing type casting is really known as an explicit type casting or explicit type conversion which is done by the programmer manually.
+//It is when a programmer changes the variable type while writing the code.
