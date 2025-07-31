43f4c0f4f1661e3f93baaeea410f5bb83cfb5201
lbarseekpangbaijr-sudo <lbarseekpangbaijr@gmail.com>
2025-07-31 11:36:21 +0200

    Fixed: issue #28
    Answered the question of issue #28 that ask to explain Unicode encoding
    and it significance to the programming language Java.

---
+++ issue28.java
@@ -0,0 +1,8 @@
+// Q.28 What is Unicode and why is it important in Java programming?
+//
+// Answer: Unicode is the universal encoding for programmers around the world,
+// similar to the ASCII encoding but it is the universal industry standard for
+// all languages and nations. As we all don't speak the same languages the
+// Unicode encoding is useful for effective communication and between
+// programmers and translation of codes from all languages to another.
+//
