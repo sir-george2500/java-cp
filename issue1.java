4956531ef62b0d1e642eef7da096b68cd8228e1c
Nimely111 <samuelehnimely@gmail.com>
2025-08-02 13:23:35 +0000

    fixed issue 1 by correctly declaring a clas Car

---
+++ issue1.java
@@ -0,0 +1,10 @@
+public class issue1{
+    public static void main(String[] args){
+
+    }
+
+    // Issue #1: Correct way to declare a class named Car in Java
+    public class Car {
+        // Class body (can be empty or contain fields, methods, constructors)
+    }
+}
