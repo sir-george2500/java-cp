c4ea7ce5f6b96a3be9a862d6b2e2588b0d99c405
Nimely111 <samuelehnimely@gmail.com>
2025-08-08 23:46:58 +0000

    initial commit for the coding_along branch

---
+++ userInput.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Date;
+import java.time.Year;
+
+public class userinput{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        Date date1 = new Date(125, 1, 12);
+        Date date2 = new Date(98, 1, 12);
+
+        int age = date1.getYear() - date2.getYear();
+            System.out.println(age);
+
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hey" + " " + name);
+
+        System.out.println("PLease enter your first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.println("PLease enter your second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Your result is: " + (num1 + num2));
+        scanner.close();
+
+    }
+}
+
+
+
+
