90680d3eaf6b9ed37b9bc91e0cfa40855c01ada6
lbarseekpangbaijr-sudo <lbarseekpangbaijr@gmail.com>
2025-08-16 10:45:48 +0200

    Fixed: issue #18
    Give the difference of the two types of data types as requested by issue
    #18.

---
+++ Solution/issue18.java
@@ -0,0 +1,12 @@
+// Q18. Explain the difference between primitive and non-primitive data types in
+// Java. Give examples.
+//
+//
+// Answer. Primitive data types are the classic and common date types that comes
+// with most programming languages e.i: int, double, char, etc. they are use to
+// store mostly numerical data types. While the non primitive data types or
+// reference data types are the data types that are methods of a class within
+// java e.i: String, etc. They are mostly created by the programmer doing the
+// coding process.
+//
+//
