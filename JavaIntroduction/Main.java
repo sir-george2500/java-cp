f8d049389c027ce7a2b42f1ef5ff1a1b3b1930bc
George S. Mulbah <gsmulbah2500@gmail.com>
2025-08-16 02:31:57 +0200

    added research about logic

--- JavaIntroduction/Main.java
+++ JavaIntroduction/Main.java
@@ -2,10 +2,25 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args){
+        int num1 = 10;
 
-        Date dNow = new Date();
+        int num2 = 20;
 
-        System.out.printf("%s %tB <%te, <%tY",dNow);
+        // !true == false
+
+        // Rust memory management
+        // Rust does not have a garbage collector, it uses a system of ownership with a set
+        // rust memory management with examp
+        // Exculsiver OR (XOR) operation
+        // This will return true if one of the conditions is true, but not both
+        if(!(num1 < num2 && num1 < 100)){
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
 
     }
+
+
+
 }
